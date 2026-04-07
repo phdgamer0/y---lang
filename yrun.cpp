12318d8dc6eec4964b064b52779429cfae368633
phdgamer0 <yazdan.phdgamer0@gmail.com>
2026-04-08 00:50:39 +0300

    planning to add a cin for file path instead of hardcoding

--- yrun.cpp
+++ yrun.cpp
@@ -1,9 +1,9 @@
 #include "old.h"
 #include "pystring.h"
 #include <fstream>
-#include <sstream>
 #include <iostream>
-void printErrorContext(const std::string&, int, int);
+#include <sstream>
+void printErrorContext(const std::string &, int, int);
 int main() {
 	/*
 	========================================================================================
@@ -16,7 +16,7 @@ int main() {
 	// -------------------------------------------------------------------------------
 	string path = "/home/phd/Desktop/CppStuff/Projects/y--lang/y---lang/test.ymm"; // <-- CHANGE THIS TO YOUR PATH!
 	// -------------------------------------------------------------------------------
-	//string path = "/home/phd/Desktop/CppStuff/Projects/y--lang/y---lang/test.ymm";
+	// string path = "/home/phd/Desktop/CppStuff/Projects/y--lang/y---lang/test.ymm";
 	std::ifstream file(path);
 	if (!file) {
 		std::cerr << "FATAL ERROR: Cannot open file at path:" << path << "\n";
@@ -31,50 +31,54 @@ int main() {
 	try {
 		auto tokens = tokenize(code);
 		Parser parser(tokens);
-		vector<Stmt*> program;
-		while (!parser.isAtEnd()) program.push_back(parser.parseStmt());
+		vector<Stmt *> program;
+		while (!parser.isAtEnd())
+			program.push_back(parser.parseStmt());
 		int useVM;
-		cout<<"Choose Compiler: AST:0 VM:1 ";
-		#ifdef VM_DEBUG_MODE
-			std::cout << "DEBUG_VM:2 ";
-		#endif
-		cin>>useVM;
+		cout << "Choose Compiler: AST:0 VM:1 ";
+#ifdef VM_DEBUG_MODE
+		std::cout << "DEBUG_VM:2 ";
+#endif
+		cin >> useVM;
 		Interpreter interp;
 		start = std::chrono::high_resolution_clock::now();
 		if (!useVM) {
 			interp.interpret(program);
 			end = std::chrono::high_resolution_clock::now();
-			std::cout<<"Program finished successfully with code 0";
-		}
-		else {
-			#ifndef VM_DEBUG_MODE
+			std::cout << "Program finished successfully with code 0";
+		} else {
+#ifndef VM_DEBUG_MODE
 			if (useVM == 2) {
 				std::cout << "\n[Notice: Fast Release Build active. Debugging disabled. Running standard VM]\n";
 				useVM = 1;
 			}
-			#endif
+#endif
 			if (useVM == 2) {
 				DEBUGGER_MODE_IS_ENABLED = true;
 			}
 			Chunk chunk;
 			ByteCodeCompiler compiler(&chunk);
-			for (Stmt* stmt : program) compiler.compileStmt(stmt);
+			for (Stmt *stmt : program)
+				compiler.compileStmt(stmt);
 			int line = program.empty() ? 0 : program.back()->line;
 			int col = program.empty() ? 0 : program.back()->col;
 			compiler.emitByte(OpCode::OP_NOTYPE, line, col);
 			compiler.emitByte(OpCode::OP_RETURN, line, col);
 			VM vm;
 			vm.globals = interp.env;
-			vm.methodResolver = [&](MethodCallExpr* m) {
+			vm.methodResolver = [&](MethodCallExpr *m) {
 				return interp.Resolve_methods(m);
 			};
 			vm.importResolver = [&](std::string libName, std::vector<std::string> symbols) {
-				if (interp.modules.find(libName) != interp.modules.end()) interp.modules[libName](interp.env, symbols);
-				else throw ImportError("Unknown module '" + libName + "'", 0, 0);
+				if (interp.modules.find(libName) != interp.modules.end())
+					interp.modules[libName](interp.env, symbols);
+				else
+					throw ImportError("Unknown module '" + libName + "'", 0, 0);
 			};
 			vm.run(chunk);
 			if (vm.globals->exists("main")) {
 				Value mainVal = vm.globals->get("main");
+				auto *funcObj = static_cast<FunctionObject *>(mainVal.ref.get());
 				if (mainVal.type == ValueType::FUNCTION) {
 					Chunk bootChunk;
 					ByteCodeCompiler bootCompiler(&bootChunk);
@@ -88,39 +92,42 @@ int main() {
 				}
 				end = std::chrono::high_resolution_clock::now();
 				std::chrono::duration<double> elapsed = end - start;
-				std::cout << "Program finished successfully in "<< elapsed.count() << " seconds "<<"with code 0";
+				std::cout << "Program finished successfully in " << elapsed.count() << " seconds " << "with code 0";
+				vm.globals->clear();
 			}
 			if (!vm.stack.empty()) {
-				//for debugging only!
-				//std::cout << "\n--- Final Stack Top ---\n";
-				//std::cout << valueToString(vm.stack.back()) << "\n";
+				// for debugging only!
+				// std::cout << "\n--- Final Stack Top ---\n";
+				// std::cout << valueToString(vm.stack.back()) << "\n";
 			}
 		}
-	}
-	catch (const LangError& e) {
+	} catch (const LangError &e) {
 		printErrorContext(code, e.line, e.col);
 		std::cerr << "\033[1;31m" << e.type << ": " << e.message << "\033[0m\n";
 		std::cerr << "Program crashed with exit code " << e.code;
-	}
-	catch (const std::exception& e) {
+	} catch (const std::exception &e) {
 		std::cerr << "Internal C++ Error: " << e.what() << "\n";
 	}
 	return 0;
 }
-void printErrorContext(const string& source, int line, int col) {
-	if (line <= 0) return;
+void printErrorContext(const string &source, int line, int col) {
+	if (line <= 0)
+		return;
 	std::stringstream ss(source);
 	string currentLineText;
 	int currentLineNum = 1;
 	while (std::getline(ss, currentLineText)) {
 		if (currentLineNum == line) {
-			if (!currentLineText.empty() && currentLineText.back() == '\r') currentLineText.pop_back();
+			if (!currentLineText.empty() && currentLineText.back() == '\r')
+				currentLineText.pop_back();
 			std::cerr << "Traceback (most recent call last):";
 			std::cerr << " File \"test.ymm\", [line: " << line << " | col: " << col << "]:" << "\n";
-			std::cerr << ::strip(currentLineText,"\t") << "\n";
+			std::cerr << ::strip(currentLineText, "\t") << "\n";
 			for (int i = 0; i < col; i++) {
-				if (i < currentLineText.size() && currentLineText[i] == '\t') continue;
-				else std::cerr << " ";
+				if (i < currentLineText.size() && currentLineText[i] == '\t')
+					continue;
+				else
+					std::cerr << " ";
 			}
 			std::cerr << "\033[1;31m^\033[0m\n";
 			break;
